1984bf24d585150fbc8016d53a7d9010fc957d18
mpnsk <mpnsk@noreply.github.com>
2022-09-27 12:26:12 +0200

    debounce hovered field

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
+import {useDebounce} from "use-debounce";
+
+// import useDebounce from "./useDebounce";
 
 function App() {
     type Point = {
         x: number
         y: number
     }
-    useEffect(()=>{
+    useEffect(() => {
         let initialState: Point[][] = [[]]
         for (let y = 0; y < 20; y++) {
             let row = []
@@ -16,27 +19,25 @@ function App() {
             initialState.push(row);
         }
         setGrid(initialState)
-    },[])
+    }, [])
     const [grid, setGrid] = useState<Point[][]>([])
     const [hoveredPoint, setHoveredPoint] = useState<Point>({x: 0, y: 0})
     const [counter, setCounter] = useState(0)
+    const [debouncedValue] = useDebounce<Point>(hoveredPoint, 200)
     return (
         <>
-            <div>hover: {hoveredPoint.x}/{hoveredPoint.y}</div>
+            <div>hover: {debouncedValue.x}/{debouncedValue.y}</div>
             <div>counter: {counter}</div>
             <div id="container"
-            onClick={event => setCounter(counter+1)}
+                 onClick={event => setCounter(counter + 1)}
             >
-                {grid.map((row,index) =>
+                {grid.map((row, index) =>
                     <span key={index}>
                         {row.map((p, index) =>
                             <span
-                                // key={p.x + '/' + p.y}
                                 key={index}
                                 onMouseEnter={() => setHoveredPoint(p)}
-                                // className={(p.x + p.y) % 2 === 0 ? 'even' : 'odd'}
                             >
-                                {/*{p.x},{p.y}*/}
                             </span>
                         )}
                     </span>
