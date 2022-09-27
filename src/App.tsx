acc5f013c63135906126c85460c9458f69c5a656
mpnsk <mpnsk@noreply.github.com>
2022-09-27 11:51:53 +0200

    add checkered colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
@@ -6,23 +6,38 @@ function App() {
         x: number
         y: number
     }
-    let initialState : Point[][] = [[]]
-    for (let y = 0; y < 20; y++) {
-        let row = []
-        for (let x = 0; x < 30; x++) {
-            row.push({x,y})
+    useEffect(()=>{
+        let initialState: Point[][] = [[]]
+        for (let y = 0; y < 20; y++) {
+            let row = []
+            for (let x = 0; x < 30; x++) {
+                row.push({x, y})
+            }
+            initialState.push(row);
         }
-        initialState.push(row);
-    }
-    const [grid, setGrid] = useState(initialState)
-
+        setGrid(initialState)
+    },[])
+    const [grid, setGrid] = useState<Point[][]>([])
+    const [hoveredPoint, setHoveredPoint] = useState<Point>({x: 0, y: 0})
+    const [counter, setCounter] = useState(0)
     return (
         <>
-            <div id="container">
-                {grid.map(row =>
-                    <span>
-                        {row.map(p =>
-                            <span>{p.x},{p.y}</span>
+            <div>hover: {hoveredPoint.x}/{hoveredPoint.y}</div>
+            <div>counter: {counter}</div>
+            <div id="container"
+            onClick={event => setCounter(counter+1)}
+            >
+                {grid.map((row,index) =>
+                    <span key={index}>
+                        {row.map((p, index) =>
+                            <span
+                                // key={p.x + '/' + p.y}
+                                key={index}
+                                onMouseEnter={() => setHoveredPoint(p)}
+                                // className={(p.x + p.y) % 2 === 0 ? 'even' : 'odd'}
+                            >
+                                {/*{p.x},{p.y}*/}
+                            </span>
                         )}
                     </span>
                 )}
