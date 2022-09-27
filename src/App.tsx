2d5cb4b6a4767410e5d4581eff592bc351265c76
mpnsk <mpnsk@noreply.github.com>
2022-09-27 11:13:57 +0200

    replace grid with dynamic grid

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,27 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-    let initialState = [
-        // as rows
-        [1, 2, 3, 4, 5, 0, 0, 0, 0, 6, 7, 8],
-        [9, 0, 1, 2, 3, 0, 0, 0, 0, 4, 5, 6],
-        [7, 8, 9, 0, 1, 0, 0, 0, 0, 2, 3, 4],
-        [5, 6, 7, 8, 9, 0, 0, 0, 0, 0, 1, 2],
-        [3, 4, 5, 6, 7, 0, 0, 0, 0, 8, 9, 0],
-        [1, 2, 3, 4, 5, 0, 0, 0, 0, 6, 7, 8],
-        [9, 0, 3, 4, 5, 0, 0, 0, 0, 6, 7, 8],
-        [5, 2, 3, 4, 5, 0, 0, 0, 0, 6, 7, 8],
-    ];
-    const [grid, setGrid] = useState(
-        initialState)
+    type Point = {
+        x: number
+        y: number
+    }
+    let initialState : Point[][] = [[]]
+    for (let y = 0; y < 20; y++) {
+        let row = []
+        for (let x = 0; x < 30; x++) {
+            row.push({x,y})
+        }
+        initialState.push(row);
+    }
+    const [grid, setGrid] = useState(initialState)
 
     return (
         <>
             <div id="container">
                 {grid.map(row =>
                     <span>
-                        {row.map(value =>
-                            <span>{value}</span>
+                        {row.map(p =>
+                            <span>{p.x},{p.y}</span>
                         )}
                     </span>
                 )}
